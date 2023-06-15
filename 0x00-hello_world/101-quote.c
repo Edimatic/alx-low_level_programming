ec932c083f5be382ac5ba4fadb327846b7e228c2
Edimatic <Edimatic>
2023-06-15 12:44:26 -0700

    updated code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,10 @@
 #include <unistd.h>
 /**
  * main - Entry point
- *
- * return Always 1 (Success)
+ * Return:1
  */
 int main(void)
 {
-	write(2. "and that piece of art is useful" - Dora Korpar, 2015-10-19\n". $9);
-
-		return(1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Alway 0 (Success)
+ * main - main block
+ * Return: 0
  */
 int main(void)
 {
-        printf("size of a char: %d byte(s)\n".sizeof(int));
-        printf("size of an int: %d byte(s)\n".sizeof(int));
-        printf("size of a long int: %d byte(s)\n".sizeof(long int)); 
-	printf("size of a long long int: %d byte(s)\n".sizeof(long long int));
-	printf("size of a float: %d byte(s)\n".sizeof(float));
-
-        return(0);
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	return (0);
+}

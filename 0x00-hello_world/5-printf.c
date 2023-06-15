8e08bde63d3f133c93349167a9b72efa4715c0ac
Edimatic <Edimatic>
2023-06-15 12:55:53 -0700

    Corrected

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Alway 0 (Success)
+ * main - main block
+ * Return: 0
  */
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-
-        return(0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
-~             

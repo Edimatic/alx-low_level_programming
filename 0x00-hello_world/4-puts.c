db9077e40f8559f88732400d576c90617b09d61b
Edimatic <Edimatic>
2023-06-15 13:01:31 -0700

    Corrected

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Alway 0 (Success)
+ * main - main block
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return(0);
-}		
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}

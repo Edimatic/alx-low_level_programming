6a73d608272a980bc7276654fd4660902e8aa961
Edimatic <Edimatic>
2023-06-16 13:12:22 -0700

    Variables

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,52 +4,46 @@
  * main - main block
  * Explanation: Write a program that prints all possible
  * different combinations of two digits
- * Numbers must be separated by ,, followed by a space
- * The two digits must be different
- * 01 and 10 are considered the same combination of the two digits 0 and 1
- * Print only the smallest combination of two digits
- * Numbers should be printed in ascending order, with two digits
- * You can only use the putchar function
- * (every other function (printf, puts, etc…) is forbidden)
- * You can only use putchar five times maximum in your code
- * You are not allowed to use any variable of type char
- * All your code should be in the main function
  * Return: 0
  */
 int main(void)
 {
-	int c;
-	int d;
-	int e = 0;
+	int c = 0;
+	int f_d;
+	int l_d;
 
-	while (e < 10)
+	int c2;
+	int f_d2;
+	int l_d2;
+
+	while (c <= 98)
 	{
-		d = 0;
-		while (d < 10)
+		f_d = (c / 10 + '0');
+		l_d = (c % 10 + '0');
+		c2 = 0;
+		while (c2 <= 99)
 		{
-			c = 0;
-			while (c < 10)
+			f_d2 = (c2 / 10 + '0');
+			l_d2 = (c2 % 10 + '0');
+
+			if (c < c2)
 			{
-				if (c != d && d != e && e < d && d < c)
+				putchar(f_d);
+				putchar(l_d);
+				putchar(' ');
+				putchar(f_d2);
+				putchar(l_d2);
+
+				if (c != 98)
 				{
-					putchar('0' + e);
-					putchar('0' + d);
-					putchar('0' + c);
-
-					if (c + d + e != 9 + 8 + 7)
-					{
-						putchar(',');
-						putchar(' ');
-					}
+					putchar(',');
+					putchar(' ');
 				}
-
-				c++;
 			}
-			d++;
+			c2++;
 		}
-		e++;
+		c++;
 	}
 	putchar('\n');
 	return (0);
 }
-

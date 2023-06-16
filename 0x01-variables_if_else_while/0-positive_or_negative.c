559f4736400dda20519d5092adf9a462e8b62165
Edimatic <Edimatic>
2023-06-16 12:34:24 -0700

    Variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main - main block
- * This program will assign a random number to the variable if the result is positive, negative or zero
- * return: 0
+ * Explanation: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0
  */
-
 int main(void)
 {
 	int n;
@@ -15,17 +14,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	{
 		printf("%i is positive\n", n);
-	}
 	else if (n < 0)
-	{
 		printf("%i is negative\n", n);
-	}
 	else
-	{
 		printf("%i is zero\n", n);
-	}
-	
+
 	return (0);
 }

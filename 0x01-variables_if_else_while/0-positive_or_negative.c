c1a3f783a233e8fb6afa444ea0935f05a8ac1c4c
Edimatic <Edimatic>
2023-06-16 09:30:40 -0700

    Variebles checked

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main - main block
- * This program will assign a random number to the variable 
- * if the result is positive, negative or zero
+ * This program will assign a random number to the variable if the result is positive, negative or zero
  * return: 0
-  */
+ */
+
 int main(void)
 {
 	int n;
@@ -14,11 +15,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
+	{
 		printf("%i is positive\n", n);
+	}
 	else if (n < 0)
+	{
 		printf("%i is negative\n", n);
+	}
 	else
+	{
 		printf("%i is zero\n", n);
+	}
 	
 	return (0);
 }

c833846b53a1e4064938291f13761819a9527870
Edimatic <Edimatic>
2023-06-20 16:25:34 -0700

    a function that adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - returns the sum of its parameters
+ * @a: int type number
+ * @b: int type number
+ * Return:0
+ */
+
+
+int add(int x, int q)
+{
+return (x + q);
+}

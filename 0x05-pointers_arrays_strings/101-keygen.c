b7fa8efc466d41e09674da7a046ab25b25bb6299
Edimatic <Edimatic>
2023-06-26 07:04:05 -0700

    Don't hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int q, add, m;
+	int pass[100];
+
+	add = 0;
+
+	srand(time(NULL));
+
+	for (q = 0; q < 100; q++)
+	{
+		pass[q] = rand() % 78;
+		add += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - add) - '0' < 78)
+		{
+			m = 2772 - add - '0';
+			add += m;
+			putchar(m + '0');
+			break;
+		}
+	}
+
+	return (0);
+}

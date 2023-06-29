651c8233002744693995583e54cc1fb0ae82fd51
Edimatic <Edimatic>
2023-06-29 08:55:29 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *_strncat - concatenates n bytes of two strings
+ * @dest: pointer destination
+ * @src: pointer source
+ * @n: number of bytes
+ * Return: void
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+	int dest_len, k;
+
+	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
+	;
+
+	for (k = 0; k < n && src[i] != '\0'; k++)
+		dest[dest_len + k] = src[k];
+
+	/*The program should end with a end of string char*/
+
+	dest[dest_len + k] = '\0';
+
+	return (dest);
+}

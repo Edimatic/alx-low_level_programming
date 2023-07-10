496ecd9ebca256c092879dff916e072fab79c863
Edimatic <Edimatic>
2023-07-10 13:31:59 -0700

    Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_HEADER_FILE
 #define MAIN_HEADER_FILE
 
+#include <stdio.h>
+#include <stdlib.h>
+
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);

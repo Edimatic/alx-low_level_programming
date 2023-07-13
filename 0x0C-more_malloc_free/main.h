9f06bc46c18f393b4b8787fa9ee6fa863730cf79
Edimatic <Edimatic>
2023-07-13 13:48:29 -0700

    Header Files

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _putchar(char c);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
+
+#endif

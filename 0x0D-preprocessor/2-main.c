2fa1cd620865e91712cf96e6d55635c064552b7b
Edimatic <Edimatic>
2023-07-17 11:39:14 -0700

    File Name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ * Explanation: a program that prints the name of the file
+ * Return: Always return 0 at the end
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}

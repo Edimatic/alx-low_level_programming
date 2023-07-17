f85ec6d067aa82d61495597fa5ca72f4fb38db9d
Edimatic <Edimatic>
2023-07-17 11:09:12 -0700

    main and putchar

---
+++ 0x0D-preprocessor/0-main.c
@@ -0,0 +1,18 @@
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * Explianation: this to show the size
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+    int siz;
+
+    siz = 98 + SIZE;
+    printf("%d\n", siz);
+    return (0);
+}

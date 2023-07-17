b9faac3060679c77accc061596862e98663bac1a
Edimatic <Edimatic>
2023-07-17 14:16:03 -0700

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,49 +1,51 @@
+#include <stdio.h>
 #include "dog.h"
 #include <stdlib.h>
-#include <stdio.h>
+#include <string.h>
 
 /**
- * struct dog - Define the dog struct
+ * new_dog - Creates a new dog and stores a copy of the name and owner.
  * @name: The name of the dog
  * @age: The age of the dog
  * @owner: The owner of the dog
+ *
+ * Return: A pointer to the new dog, or NULL if allocation fails
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *new_dog;
+	char *name_copy;
+	char *owner_copy;
 
-	int i = 0, j = 0, k;
-	dog_t *doge;
+	/* Allocate memory for the new dog */
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
 
-	while (name[i] != '\0')
-		i++;
-	while (owner[j] != '\0')
-		j++;
-	doge = malloc(sizeof(dog_t));
-	if (doge == NULL)
+	/* Allocate memory for copying the name string */
+	name_copy = malloc(strlen(name) + 1);
+	if (name_copy == NULL)
 	{
-		free(doge);
+		free(new_dog);
 		return (NULL);
 	}
-	doge->name = malloc(i * sizeof(doge->name));
-	if (doge->name == NULL)
+	strcpy(name_copy, name);
+
+	/* Allocate memory for copying the owner string */
+	owner_copy = malloc(strlen(owner) + 1);
+	if (owner_copy == NULL)
 	{
-		free(doge->name);
-		free(doge);
+		free(name_copy);
+		free(new_dog);
 		return (NULL);
 	}
-	for (k = 0; k <= i; k++)
-		doge->name[k] = name[k];
-	doge->age = age;
-	doge->owner = malloc(j * sizeof(doge->owner));
-	if (doge->owner == NULL)
-	{
-		free(doge->owner);
-		free(doge->name);
-		free(doge);
-		return (NULL);
-	}
-	for (k = 0; k <= j; k++)
-		doge->owner[k] = owner[k];
-	return (doge);
+	strcpy(owner_copy, owner);
+
+	/* Set the fields of the new dog */
+	new_dog->name = name_copy;
+	new_dog->age = age;
+	new_dog->owner = owner_copy;
+
+	/* Return the pointer to the new dog */
+	return (new_dog);
 }

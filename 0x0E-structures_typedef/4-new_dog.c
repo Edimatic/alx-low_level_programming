71ae9caa48d053c0b5cfcf811b2cadcf7b623c21
Edimatic <Edimatic>
2023-07-17 10:27:28 -0700

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,50 +3,47 @@
 #include <stdio.h>
 
 /**
- * new_dog - new dog struct
- * @name: name's dog
- * @age: age's dog
- * @owner: owner's dog
- * Return: New_dog at end
+ * struct dog - Define the dog struct
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 
-	int x = 0, h = 0, m;
+	int i = 0, j = 0, k;
 	dog_t *doge;
 
-	while (name[x] != '\0')
-		x++;
-	while (owner[h] != '\0')
-		x++;
-	/* Allocate memory for the new dog */
+	while (name[i] != '\0')
+		i++;
+	while (owner[j] != '\0')
+		j++;
 	doge = malloc(sizeof(dog_t));
 	if (doge == NULL)
 	{
 		free(doge);
 		return (NULL);
 	}
-	doge->name = malloc(x * sizeof(doge->name));
+	doge->name = malloc(i * sizeof(doge->name));
 	if (doge->name == NULL)
 	{
 		free(doge->name);
 		free(doge);
 		return (NULL);
 	}
-	for (m = 0; m <= x; m++)
-		doge->name[m] = name[m];
+	for (k = 0; k <= i; k++)
+		doge->name[k] = name[k];
 	doge->age = age;
-	doge->owner = malloc(h * sizeof(doge->owner));
+	doge->owner = malloc(j * sizeof(doge->owner));
 	if (doge->owner == NULL)
 	{
-	/* Don't forget to free the memory when you are done with the dog */
 		free(doge->owner);
 		free(doge->name);
 		free(doge);
 		return (NULL);
 	}
-	for (m = 0; m <= h; m++)
-		doge->owner[m] = owner[m];
+	for (k = 0; k <= j; k++)
+		doge->owner[k] = owner[k];
 	return (doge);
 }

db0989ce02a3b8245b0701ba8f6cf3738f427d35
Edimatic <Edimatic>
2023-07-20 04:07:31 -0700

    C-Variadic Function Headers

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,28 @@
+#ifndef VARIADIC_HEADER_FILE
+#define VARIADIC_HEADER_FILE
+#include <stdarg>
+
+int sum_them_all(const unsigned int n, ...);
+int _putchar(char c)
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+void print_int(va_list list);
+void print_float(va_list list);
+void print_char(va_list list);
+void print_str(va_list list);
+
+/**
+ * struct printTypeStruct - structure definition of a printTypeStruct
+ * @type: type
+ * @printer: function to print
+ */
+typedef struct printTypeStruct
+{
+char *type;
+void (*printer)(va_list);
+} printTypeStruct;
+
+
+#endif

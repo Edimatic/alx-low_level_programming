84c4ea5ce92f372defd6e55dd8efc9e24ff58cbd
Edimatic <ediomiudofia@gmail.com>
2023-07-27 16:17:41 -0700

    Real programmers can write assembly code in any language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,23 +1,9 @@
-section .data
-    hello_msg db "Hello, Holberton", 0
-    format db "%s", 10, 0    ; %s format for a null-terminated string followed by newline (10)
-
-section .text
-    global main
-
-extern printf
-
+          global    main
+          extern    printf
 main:
-    mov rdi, format
-    mov rsi, hello_msg
-    xor rax, rax         ; RAX = 0, indicating that we will pass arguments to printf using the registers
-    call printf
-
-    ; Exit the program
-    xor rdi, rdi
-    call exit
-
-section .bss
-    ; Reserve some space for the exit function to work
-    resb 8
-
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0

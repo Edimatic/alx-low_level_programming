8e4cb7ad7c98d4fa35eff414eaeaf4e5d0c724a0
Edimatic <ediomiudofia@gmail.com>
2023-07-31 12:27:32 -0700

    Print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - Print elements of a singly linked list.
+ * @h: Pointer to a list.
+ * Return: Integer at the end
+ **/
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *arv;
+	unsigned int cont = 0;
+
+	arv = h;
+	while (arv)
+	{
+		printf("%d\n", arv->n);
+		cont++;
+		arv = arv->next;
+	}
+	return (cont);
+}

4d15151d3faf9c67d6e3cc4dcb9e1d829ea4bc2a
Edimatic <ediomiudofia@gmail.com>
2023-07-31 16:10:27 -0700

    Add node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -6,34 +6,34 @@
  * @head: Address of the first node of a list.
  * @n: Integer to insert into the new node.
  *
- * Description: This function adds a new node at the beginning of a linked list.
- *              It allocates memory for the new node and sets its integer value.
+ * Description: This function adds a new node.
+ *              It allocates memory for the new node.
  *              The new node becomes the new head of the list.
  *
- * Return: The address of the new node (new head), or NULL if memory allocation fails.
+ * Return: The address of the new nod, or NULL if memory allocation fails.
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-    listint_t *new_node;
+	listint_t *new_node;
 
     /* Allocate memory for the new node */
-    new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL)
-    {
-        /* Memory allocation failed */
-        return (NULL);
-    }
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+{
+	/* Memory allocation failed */
+	return (NULL);
+}
 
     /* Set the integer value of the new node */
-    new_node->n = n;
+	new_node->n = n;
 
     /* Make the new node point to the current head */
-    new_node->next = *head;
+	new_node->next = *head;
 
     /* Update the head pointer to point to the new node (new head) */
-    *head = new_node;
+	*head = new_node;
 
     /* Return the address of the new node (new head) */
-    return (*head);
+	return (*head);
 }
 

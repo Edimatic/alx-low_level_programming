a8f2da7e5725bd78f679c59820197b0c84b92da4
Edimatic <ediomiudofia@gmail.com>
2023-08-01 14:35:18 -0700

    Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,36 +1,29 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * print_listint_safe - Prints a listint_t linked list safely.
- * @head: Pointer to the first node in the linked list.
+ * print_listint_safe - prints a linked list, safely
+ * @head: list of type listint_t to print
  *
- * Return: The number of nodes in the list.
+ * Return: number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current = head;
-	const listint_t *loop_node = NULL;
-	size_t node_count = 0;
+	size_t num = 0;
+	long int diff;
 
-	while (current)
+	while (head)
 	{
-		printf("[%p] %d\n", (void *)current, current->n);
-		node_count++;
-		current = current->next;
-
-	/* Check if the current node is repeated (loop detected) */
-		if (current == loop_node)
+		diff = head - head->next;
+		num++;
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (diff > 0)
+			head = head->next;
+		else
 		{
-			printf("-> [%p] %d\n", (void *)current, current->n);
-			exit(98); /* Exit the program with status 98 */
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
 		}
-
-	/* Set the loop_node to the current node to detect loops */
-		loop_node = current;
 	}
 
-	return (node_count);
+	return (num);
 }
-

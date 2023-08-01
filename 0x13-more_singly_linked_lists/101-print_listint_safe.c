0d42e8e5741099fb63652ed05a55ac9508cc7599
Edimatic <ediomiudofia@gmail.com>
2023-08-01 15:07:08 -0700

    Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,29 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * print_listint_safe - prints a linked list, safely
- * @head: list of type listint_t to print
+ * print_listint_safe - Prints a linked list, safely.
+ * @head: Pointer to the list of type listint_t to print.
  *
- * Return: number of nodes in the list
+ * Return: Number of nodes in the list.
  */
+
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t num = 0;
-	long int diff;
+	size_t num_nodes = 0;
+	long int address_diff;
 
 	while (head)
 	{
-		diff = head - head->next;
-		num++;
+		/* Calculate the difference in addresses to detect loops */
+
+		address_diff = head - head->next;
+		num_nodes++;
+
+		 /* Print the address and value of the current node */
+
 		printf("[%p] %d\n", (void *)head, head->n);
-		if (diff > 0)
-			head = head->next;
+
+		if (address_diff > 0)
+			head = head->next; /* Move to the next node if no loop detected */
 		else
 		{
+
+		/* Print the address and value of the node where the loop starts*/
+
 			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
 			break;
 		}
 	}
 
-	return (num);
+	/* Return the number of nodes in the list */
+
+	return (num_nodes);
 }
+

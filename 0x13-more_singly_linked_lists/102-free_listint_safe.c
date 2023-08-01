ed9519666bc0987654116f22a427b50c86ae0bda
Edimatic <ediomiudofia@gmail.com>
2023-08-01 15:50:03 -0700

    Free (safe version)

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,24 +1,58 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * free_listint_safe - Frees a linked list.
- * @h: Pointer to the first node in the linked list.
+ * free_listint_safe - Frees a linked list, safely.
+ * @head: Pointer to the first node in the linked list.
  *
- * Return: Number of elements in the freed list.
+ * Description:
+ * This function safely frees a linked list of type listint_t. It iterates
+ * through the list and releases the memory occupied by each node while
+ * handling the cases where a loop is present in the list.
+ *
+ * Return: The number of elements in the freed list.
  */
-size_t free_listint_safe(listint_t **h)
+size_t free_listint_safe(listint_t **head)
 {
-	size_t len = 0;
-	int diff;
+	size_t num_freed_nodes = 0; /* Variable to store the number of nodes freed */
+	int address_diff; /* Variable to calculate the difference in addresses */
+	listint_t *current; /* Pointer to the current node being freed */
+	listint_t *next_node; /* Temporary pointer to hold the next node deeping */
 
-	while (h && *h)
+	/* Check if the list is NULL or empty */
+	if (!head || !*head)
+		return (0);
+
+	/* Loop to traverse the list and free the nodes */
+
+	while (*head)
 	{
-		diff = *h - (*h)->next;
-		*h = (diff > 0) ? ((*h)->next) : NULL;
-		len++;
+
+		/* Calculate the difference in addresses to detect loops */
+		address_diff = *head - (*head)->next;
+
+		/* Check if no loop is detected (positive address difference) */
+
+		if (address_diff > 0)
+		{
+
+		current = *head; /* Store the current node in 'current' to free it */
+	next_node = (*head)->next; /*Store the nxt node in 'next_node' to avoid lost*/
+	free(current); /* Free the current node */
+	*head = next_node; /* Move 'head' to the next node in the list */
+	num_freed_nodes++; /* Increment the number of nodes freed */
+		}
+		else /* Loop detected or single node list */
+		{
+			*head = NULL; /* Set 'head' to NULL to break out of the loop safely */
+			num_freed_nodes++; /* Increment the number of nodes freed */
+			break;
+		}
 	}
 
-	return (len);
+	*head = NULL; /* Set the head pointer to NULL (list is now empty) */
+
+	return (num_freed_nodes); /* Return the total number of nodes freed */
 }
 

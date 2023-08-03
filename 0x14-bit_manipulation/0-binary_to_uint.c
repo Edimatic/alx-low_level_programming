a3f3028eb4c74d997668186d3fa210303da36068
Edimatic <ediomiudofia@gmail.com>
2023-08-03 11:14:20 -0700

    function that converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,25 +9,25 @@
  */
 unsigned int _binary_string_to_unsigned_int(const char *binary_string)
 {
-        int index;
-        unsigned int result;
+	int index;
+	unsigned int result;
 
-        result = 0;
-        if (!binary_string)
-        return (0);
+	result = 0;
+	if (!binary_string)
+	return (0);
 
-        for (index = 0; binary_string[index] != '\0'; index++)
-        {
-        if (binary_string[index] != '0' && binary_string[index] != '1')
-                return (0);
-        }
+	for (index = 0; binary_string[index] != '\0'; index++)
+	{
+	if (binary_string[index] != '0' && binary_string[index] != '1')
+	return (0);
+	}
 
-        for (index = 0; binary_string[index] != '\0'; index++)
-        {
-                result <<= 1;
-                if (binary_string[index] == '1')
-                        result += 1;
-        }
-        return (result);
+	for (index = 0; binary_string[index] != '\0'; index++)
+	{
+		result <<= 1;
+		if (binary_string[index] == '1')
+		result += 1;
+	}
+	return (result);
 }
 

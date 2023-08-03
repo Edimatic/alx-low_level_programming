0d720d38d567bf643077c9025557efb6f48747d7
Edimatic <ediomiudofia@gmail.com>
2023-08-03 05:59:18 -0700

    converts a binary number to an unsigned int

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,10 +2,10 @@
 #include "holberton.h"
 
 /**
- * _binary_string_to_unsigned_int - Converts a binary string to an unsigned int.
- * @binary_string: A pointer to a string containing a binary number.
+ * _binary_string_to_unsigned_int - Converts a binary string to an unsigned int
+ * @binary_string: A pointer to a string containing a binary number
  *
- * Return: An unsigned int with the decimal value of the binary number, or 0 if there's an error.
+ * Return: An unsigned int with the decimal value of the binary number, or 0 if there's an error
  */
 unsigned int _binary_string_to_unsigned_int(const char *binary_string)
 {
